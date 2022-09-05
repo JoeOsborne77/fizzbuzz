8928041b48380d4ba57077c50544d4307d85a66d
Joe Osborne <joeosborne77@gmail.com>
2022-09-05 16:43:30 +0100

    FizzBuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,13 @@
-def fizzbuzz(number)
-  'fizz'  
-end
+def fizzbuzz(num)
+    if num % 3 == 0 && num % 5 == 0
+      return 'FizzBuzz'
+    elsif num % 5 == 0
+      return 'Buzz'
+    elsif num % 3 == 0
+      return 'Fizz'
+    else return num 
+    end
+  end
+  
+  x = (0..100).to_a
+  x.each { |i| puts fizzbuzz(i) }
